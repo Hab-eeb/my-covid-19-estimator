2243372d445ffd1b03937f4e35eab5237f97c759
Habeebullah Agbaje <agbaj@Habeeb-PC.beeb>
2020-04-08 09:53:38 +0100

    First challenge all inside covid function

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const impact = {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0
+  };
+  const severeImpact = {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0
+  };
+
+  const day = 30;
+  let currInfI = impact.currentlyInfected;
+  let currInfS = severeImpact.currentlyInfected;
+  currInfI = input.reportedCases * 10;
+  currInfS = input.reportedCases * 50;
+
+  impact.infectionsByRequestedTime = currInfI * (2 ** (day / 3));
+  severeImpact.infectionsByRequestedTime = currInfS * (2 ** (day / 3));
+
+  return {
+    data: input,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
