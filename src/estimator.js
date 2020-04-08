0e5c9bcccb227bf481a746ae228bc88bac7ca352
Habeebullah Agbaje <agbaj@Habeeb-PC.beeb>
2020-04-08 10:26:34 +0100

    edit the day

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,7 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  const Week = 'week';
+  const Months = 'months';
 
   const impact = {
     currentlyInfected: 0,
@@ -9,15 +11,24 @@ const covid19ImpactEstimator = (data) => {
     currentlyInfected: 0,
     infectionsByRequestedTime: 0
   };
-
-  const day = 30;
+  const perType = input.periodType;
+  const day = input.timeToElapse;
+  let days = 0;
   let currInfI = impact.currentlyInfected;
   let currInfS = severeImpact.currentlyInfected;
   currInfI = input.reportedCases * 10;
   currInfS = input.reportedCases * 50;
 
-  impact.infectionsByRequestedTime = currInfI * (2 ** (day / 3));
-  severeImpact.infectionsByRequestedTime = currInfS * (2 ** (day / 3));
+  if (perType === Months) {
+    days = 30 * day;
+  } else if (perType === Week) {
+    days = 7 * day;
+  } else {
+    days = day;
+  }
+
+  impact.infectionsByRequestedTime = currInfI * (2 ** (days / 3));
+  severeImpact.infectionsByRequestedTime = currInfS * (2 ** (days / 3));
 
   return {
     data: input,
